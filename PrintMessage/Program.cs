bb6efc112030f0b50ee7a716f1a67bb261369e2a
Jason Farrell <imjason@gmail.com>
2021-02-04 14:07:31 -0500

    initial commit

---
+++ PrintMessage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Azure.Storage.Queues;
+
+namespace PrintMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            const string queueName = "test-queue";
+
+            QueueClient client = new QueueClient(connectionString, queueName);
+            var message = client.ReceiveMessage();
+
+            if (message.Value != null)
+            {
+                Console.WriteLine(message.Value.Body);
+                client.DeleteMessage(message.Value.MessageId, message.Value.PopReceipt);
+            }
+            else
+            {
+                Console.WriteLine("No message");
+            }
+
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ SendMessage/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+
+namespace SendMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            const string queueName = "test-queue";
+            int numberOfMessagesToSend = int.Parse(args[0]);
+
+            Console.WriteLine($"Sending {numberOfMessagesToSend} to Queue");
+
+            var random = new Random();
+            var inc = 0;
+
+            QueueClient client = new QueueClient(connectionString, queueName);
+            var tasks = new List<Task>();
+
+            while (inc < numberOfMessagesToSend)
+            {
+                tasks.Add(client.SendMessageAsync($"Random number is {random.Next(1000)}"));
+                inc++;
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("Send Complete");
+        }
+    }
+}
